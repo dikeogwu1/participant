f4ce816e49d5656f28647841da0eb3ea7164545a
fabian <godsonhm3@gmail.com>
2023-09-10 22:48:37 +0100

    fix UTC time and enhance UI

--- app.js
+++ app.js
@@ -12,6 +12,23 @@ const days = [
 ];
 
 // Insert current day
-currentDay.textContent = days[new Date().getDay()];
-// Insert UTC time
-currentUTCTime.textContent = Date.now();
+currentDay.textContent = `Current Day of the Week: ${
+  days[new Date().getDay()]
+}`;
+
+// Get UTC time
+function getUTC() {
+  const UTC = new Date();
+  // Calculate milliseconds
+  const utcOffset = 1 * 60 * 60 * 1000; // UTC+1
+  // Apply the UTC+1 offset
+  const time = new Date(UTC.getTime() + utcOffset);
+  // create UTC time
+  const hours = time.getUTCHours().toString().padStart(2, "0");
+  const minutes = time.getUTCMinutes().toString().padStart(2, "0");
+  const seconds = time.getUTCSeconds().toString().padStart(2, "0");
+  // insert UTC time
+  currentUTCTime.textContent = `UTC Time: ${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(getUTC, 1000);
